506dff6c3dcfef4dbdef560a79c893207f1a7fbc
Sachini Siriwardhana <sachini@revox.io>
2025-07-03 12:11:41 +0530

    feat: Add description column to todo table

--- prisma/migrations/20250625084136_init/migration.sql
+++ prisma/migrations/20250625084136_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `todos` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
+    `description` TEXT,
     `completed` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
